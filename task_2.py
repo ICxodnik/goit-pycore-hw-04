d89d2387dec11d8b9c283fcd898ff3d062971866
Natalia <icxodnik@gmail.com>
2025-03-27 15:26:45 +0100

    refactoring of task 2

--- task_2.py
+++ task_2.py
@@ -2,21 +2,24 @@ from pathlib import Path
 
 FILENAME = Path(__file__).parent / "task_2_data.txt"
 
+def get_cats_info(string_data: str):
+    cats_lines = []
+    for line in string_data:
+        parts = line.strip().split(",")
+        if len(parts) == 3:  # Переконуємося, що є три елементи
+            try:
+                cats_lines.append({"cat_id": parts[0], "name": parts[1], "age": int(parts[2])})
+            except ValueError:
+                print(f"Помилка перетворення віку в число: {parts[2]}")
+        else:
+            print(f"Некоректний рядок (пропускаємо): {line.strip()}")
+    return cats_lines
     
-def parseFile(filename):
+def parseFile(filename)-> str: 
     try:
         with open(filename, 'r', encoding='utf-8') as file:
-            cats_lines = []
-            for line in file:
-                parts = line.strip().split(",")
-                if len(parts) == 3:  # Переконуємося, що є три елементи
-                    try:
-                        cats_lines.append({"cat_id":parts[0], "name": parts[1], "age": int(parts[2])})
-                    except ValueError:
-                        print(f"Помилка перетворення віку в число: {parts[2]}")
-                else:
-                    print(f"Некоректний рядок (пропускаємо): {line.strip()}")
-            return cats_lines
+            return file.readlines()
+            #lines = [el.strip() for el in file.readlines()]
     except FileNotFoundError:
         print("Файл не знайдено.")
         return []
@@ -25,6 +28,7 @@ def parseFile(filename):
         return []
 
 if __name__ == '__main__':
-    items = parseFile(FILENAME)
+    data = parseFile(FILENAME)
+    cats = get_cats_info(data)
 
-    print(f"Список котів: {items}")
+    print(f"Список котів: {cats}")
