5d05d15c77549c911e3f9baf7b9dbbe6b5647f21
Natalia <icxodnik@gmail.com>
2025-03-27 14:48:57 +0100

    refactoring of the first task

--- task_1.py
+++ task_1.py
@@ -2,22 +2,27 @@ from pathlib import Path
 
 FILENAME = Path(__file__).parent / "task_1_data.txt"
 
-
-def total_salary(path):
+def total_salary(items):
+    total = sum(items)
+    average = total / len(items)
+    return total, average
+    
+def parseFile(filename):
     try:
-        with open(path, 'r', encoding='utf-8') as file:
+        with open(filename, 'r', encoding='utf-8') as file:
             lines = [el.strip().split(",")[1] for el in file.readlines()]
             int_lines = list(map(int, lines))
-
-            total = sum(int_lines)
-            average = total / len(int_lines)
-            return total, average 
+            return int_lines
     except FileNotFoundError:
         print("Файл не знайдено.")
-        return None, None
+        return None
     except Exception as e:
         print(f"Помилка: {e}")
-        return None, None
+        return None
 
 if __name__ == '__main__':
-    print(total_salary(FILENAME))
+    items = parseFile(FILENAME)
+
+    total, average = total_salary(items)
+
+    print(total, average)
